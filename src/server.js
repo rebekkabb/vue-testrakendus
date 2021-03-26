eba220e33ae1dee8381708956ae021c4e94b2fbe
Rebekka Breedis <breedis.rebekka@gmail.com>
2021-03-27 00:44:38 +0200

    created endpoints in mirage

--- src/server.js
+++ src/server.js
@@ -19,6 +19,26 @@ export function makeServer({environment = "development"} = {}) {
             this.get("/recipes", (schema) => {
                 return schema.recipes.all()
             })
+
+            this.post("/recipes", (schema, request) => {
+                let attrs = JSON.parse(request.requestBody)
+
+                return schema.recipes.create(attrs)
+            })
+
+            this.patch("/recipes/:id", (schema, request) => {
+                let attrs = JSON.parse(request.requestBody)
+                let id = request.params.id
+                let recipe = schema.recipes.find(id)
+
+                return recipe.update(attrs)
+            })
+
+            this.delete("/recipes/:id", (schema, request) => {
+                let id = request.params.id
+
+                return schema.recipes.find(id).destroy()
+            })
         },
     })
 
