65235baaa69ba224964c07c3861e0e4561a092e2
Rebekka Breedis <breedis.rebekka@gmail.com>
2021-03-27 00:45:23 +0200

    implemented logic

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,95 @@
 <template>
   <div id="app">
-    <div class="flex flex-row">
-      <div>
-        <ul id="recipes">
-          <li v-for="recipe in recipes" v-bind:key="recipe.id" :currentRecipe="recipe">{{ recipe.title }}</li>
-        </ul>
+    <div class="container">
+      <div class="recipe-list">
+        <h1> Retseptid </h1>
+        <button @click="swapComponent('add', '')"> Lisa retsept</button>
+        <div id="recipes" v-for="recipe in recipes" v-bind:key="recipe.id">
+          <RecipeItem :recipe="recipe"
+                      v-on:swapComponent="swapComponent('display', recipe)"
+                      v-on:delete="deleteRecipe(recipe)"
+                      v-on:edit="swapComponent('edit', recipe)"></RecipeItem>
+        </div>
       </div>
+      <component :is="currentComponent" v-bind="{ recipe: this.currentRecipe}"
+                 v-on:swap="swapComponent('intro', '')"></component>
     </div>
   </div>
+
 </template>
 
 <script>
 
+import RecipeDisplay from "./components/RecipeDisplay";
+import RecipeItem from "./components/RecipeItem";
+import IntroDisplay from "./components/IntroDisplay";
+import RecipeAddForm from "./components/RecipeAddForm";
+import RecipeEditForm from "./components/RecipeEditForm";
+import axios from 'axios';
+
 export default {
   name: 'App',
+  components: {
+    RecipeItem,
+    'display': RecipeDisplay,
+    'intro': IntroDisplay,
+    'add': RecipeAddForm,
+    'edit': RecipeEditForm
+  },
   data() {
     return {
       recipes: [],
-      currentRecipe: {}
+      currentRecipe: null,
+      currentComponent: 'intro'
     }
   },
+  methods: {
+    listRecipes() {
+      axios.get('/api/recipes')
+          .then(function (response) {
+            this.recipes = response.data.recipes
+          }.bind(this));
+    },
+    swapComponent: function (component, recipe) {
+      this.currentComponent = component
+      this.currentRecipe = recipe
+      this.listRecipes()
+    },
+    deleteRecipe: function (recipe) {
+      fetch("/api/recipes/" + recipe.id, {method: 'delete', body: JSON.stringify(recipe)})
+          .then(res => res.json())
+          .then(data => {
+            alert(data.message)
+          })
+      this.swapComponent('intro', '')
+    },
+  },
   created() {
-    fetch("/api/recipes")
-        .then((res) => res.json())
-        .then((json) => {
-          this.recipes = json.recipes
-        })
-  }
+    this.listRecipes()
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+}
+
+.container {
+  display: flex;
+  flex-direction: row;
 }
 </style>
